03156b4a6701d3ca9536a9b81ddea95b4669b5b1
Patrik <gazdagpatrik99@gmail.com>
2022-09-14 15:59:41 +0200

    Lambda

---
+++ lambda.py
@@ -0,0 +1,47 @@
+def square(num):
+    return num**2
+
+my_nums=[1,2,3,4,5]
+
+for item in map(square,my_nums):
+    print(item)
+
+print(list(map(square,my_nums)))
+
+############################
+def splicer(mystring):
+    if len(mystring)%2 == 0:
+        return 'Even'
+    else:
+        return mystring[0]
+
+names = ['Andy','Eve','Sally']
+
+print(list(map(splicer,names)))
+
+############################
+my_nums=[1,2,3,4,6]
+
+def check_even(num):
+    return num%2 == 0
+
+print(list(filter(check_even,my_nums)))
+
+for n in filter(check_even,my_nums):
+    print(n)
+
+############################
+def square(num):
+    result = num ** 2
+    return result
+
+print(square(3))
+
+square = lambda num: num ** 2
+print(square(5))
+
+print(list(map(lambda num:num**2,my_nums)))
+
+print(list(filter(lambda num:num%2 == 0,my_nums)))
+
+print(list(map(lambda x:x[::-1],names)))
