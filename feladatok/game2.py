4655d910e4eae2bdcedc1cc552737db28573866f
Patrik <gazdagpatrik99@gmail.com>
2022-09-28 16:03:27 +0200

    XorO

--- feladatok/game2.py
+++ feladatok/game2.py
@@ -1,23 +1,64 @@
+from contextlib import nullcontext
 import numpy as np
-x =  np.empty(shape=(11,5),dtype=str)
+
+x =  np.empty(shape=(5,5),dtype=str)
+
+def position(value,db):
+    if db % 2 != 0:
+        print("Player 2 will come")
+        db += 1
+    elif db != 0:
+        print("Player 1 will come")
+        db += 1
+    else:
+        pass
+
+    within_range = False
+    while within_range==False:
+        pos = input("Choose your next position (1-9): ")
+        if pos.isdigit() == False:
+            print("Sorry that is not a digit!")
+        else:
+            if int(pos) in range(1,10):
+                within_range = True
+            else:
+                print("Not in range")
+                position(value,db)
+    if int(pos) == 1:
+        x[0][0] = value
+    elif int(pos) == 2:
+        x[0][2] = value
+    elif int(pos) == 3:
+        x[0][4] = value
+    elif int(pos) == 4:
+        x[2][0] = value
+    elif int(pos) == 5:
+        x[2][2] = value
+    elif int(pos) == 6:
+        x[2][4] = value
+    elif int(pos) == 7:
+        x[4][0] = value
+    elif int(pos) == 8:
+        x[4][2] = value
+    elif int(pos) == 9:
+        x[4][4] = value
+    print(x)
+    position(value,db)
+
 def format_board():
     idxo=0
-    while idxo < 11:
-        if idxo % 2 != 0:
-            x[idxo][1] = "|"
-            x[idxo][3] = "|"
-            idxo += 1
-        else:
-            x[idxo][0] = " "
-            x[idxo][2] = " "
-            x[idxo][4] = " "
-            idxo += 1
+    while idxo < 5:
+        x[idxo][1] = "|"
+        x[idxo][3] = "|"
+        x[idxo][0] = " "
+        x[idxo][2] = " "
+        x[idxo][4] = " "
+        idxo += 1
     idxs=0
     while idxs < 5:
+        x[1][idxs] = "-"
         x[3][idxs] = "-"
-        x[7][idxs] = "-"
         idxs += 1
-    print(x)
 
 def display():
     print("Welcome to Tic Tac Toe!")
@@ -38,13 +79,20 @@ def ready_to_start():
         ready_to_start()
     if ready == "Yes":
         print(x)
+        return ready
     elif ready == "No":
         print("The game has over.")
+        return ready
 
 def game():
+    db = 0
+    format_board()
     display()
-    XorO()
-    ready_to_start()
+    result = XorO()
+    ready = ready_to_start()
+    if ready == "Yes":
+        position(result,db)
+    else:
+        pass
 
-#game()
-format_board()
+game()
