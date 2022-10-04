4c77107eda9c273a34cd207ade677873fcfc959b
Patrik <gazdagpatrik99@gmail.com>
2022-10-04 15:58:51 +0200

    Game on

--- feladatok/war_game.py
+++ feladatok/war_game.py
@@ -1,5 +1,3 @@
-from cgitb import reset
-from hashlib import new
 import random
 
 # List of Suits
@@ -41,15 +39,61 @@ class Deck:
     def deal_one(self):
         return self.all_cards.pop()
 
-new_deck = Deck()
-new_deck.shuffle()
-mycard = new_deck.deal_one()
-
-print(mycard)
-print(len(new_deck.all_cards))
-
-
+# print(mycard)
+# print(len(new_deck.all_cards))
 
 class Player:
-    def __init__(self):
-        pass
+    def __init__(self,name):
+        self.name = name
+        self.all_cards = []
+
+    def remove_one(self):
+        self.all_cards.pop(0)
+
+    def add_cards(self,new_cards):
+        if type(new_cards) == type([]):
+            # List of multiple Cards objects
+            self.all_cards.extend(new_cards)
+        else:
+            # Far a single Card object
+            self.all_cards.append(new_cards)
+
+    def __str__(self):
+        return f'Player {self.name} has {len(self.all_cards)} cards.'
+
+new_player = Player("Patrizio")
+new_player.add_cards([mycard,mycard,mycard])
+print(new_player)
+
+# Game Setup
+player_one = Player("One")
+player_two = Player("Two")
+
+new_deck = Deck()
+new_deck.shuffle()
+
+for x in range(26):
+    player_one.add_cards(new_deck.deal_one())
+    player_two.add_cards(new_deck.deal_one())
+
+game_on = True
+round_num = 0
+
+while game_on:
+    round_num += 1
+    print(f'Round {round_num}')
+    if len(player_one.all_cards) == 0:
+        print('Player One, out of cards! Player Two Wins!')
+        game_on = False
+        break
+    if len(player_two.all_cards) == 0:
+        print('Player Two, out of cards! Player One Wins!')
+        game_on = False
+        break
+
+    player_one_cards = []
+    player_one_cards.append(player_one.remove_one())
+    player_two_cards = []
+    player_one_cards.append(player_two.remove_one())
+    
+    # while at war
